0a1f635c84b62656ad432f87b796cea5096aadd4
keita hino <keitahino0415@gmail.com>
2021-08-28 16:30:32 +0900

    vue-tsc実行

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import {info, setFailed, setOutput} from '@actions/core'
+import {exec} from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
     const workingDir = process.cwd()
     info(`working directory: ${workingDir}`)
+
+    // TODO:npmかyarnかのチェックを入れる
+    await exec('npm install -g vue-tsc@0.0.25')
+    await exec('$(npm root -g)/vue-tsc/vue-tsc.js --pretty false --noEmit')
+    info('実行終わり')
     setOutput('time', new Date().toTimeString())
   } catch (error) {
     setFailed(error.message)
